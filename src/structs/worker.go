114bdb91e9f7ae91448fcb9ee56351f0e42f551e
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-03-23 12:16:09 +0100

    dokończony worker

--- src/structs/worker.go
+++ src/structs/worker.go
@@ -1,8 +1,19 @@
 package structs
 
+import "time"
+
 type Worker struct {
-	Delay int
-	Tasks chan Task
+	Delay   int
+	Tasks   chan Task
 	Results chan int
 }
 
+func (worker *Worker) Start() {
+	for {
+		time.Sleep(time.Duration(worker.Delay))
+		t := <-worker.Tasks
+		if r, e := t.ResolveTask(); e != nil {
+			worker.Results <- r
+		}
+	}
+}
