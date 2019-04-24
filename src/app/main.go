cf0692a952f8195dcf441ef99d27b6d9a58be14c
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-04-24 16:05:08 +0200

    działająca wersja

--- src/app/main.go
+++ src/app/main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-	"conf"
+	"PWGo/src/conf"
+	"PWGo/src/routines"
 	"fmt"
-	"routines"
+	"math/rand"
 )
 
 func main() {
 	var taskListState = make(chan interface{})
 	var itemListState = make(chan interface{})
+	var workersStates = make([]chan interface{}, 0)
+
+	for i := 0; i < conf.Workers; i++ {
+		var channel = make(chan interface{})
+		workersStates = append(workersStates, channel)
+	}
 
 	var ceoToTaskList = make(chan routines.Task)
 	var taskListToWorkers = make(chan chan routines.Task)
 	var workersToItemList = make(chan routines.Item)
 	var itemListToClients = make(chan chan routines.Item)
 
+	var machines = make([]chan chan routines.Task, 0)
+	for i := 0; i < conf.Machines; i++ {
+		var machineChan = make(chan chan routines.Task)
+		machines = append(machines, machineChan)
+		go routines.Machine(routines.MachineConfig{i, conf.Verbose, conf.DelayMachine}, machineChan)
+	}
+
 	go routines.Tasks(conf.TaskListSize, ceoToTaskList, taskListToWorkers, taskListState)
 	go routines.Items(conf.ItemListSize, workersToItemList, itemListToClients, itemListState)
 
 	for i := 0; i < conf.Workers; i++ {
-		go routines.Worker(i, conf.Verbose, taskListToWorkers, workersToItemList, conf.DelayWorker)
+		go routines.Worker(routines.WorkerConfig{
+			Id:      i,
+			Verbose: conf.Verbose,
+			Delay:   conf.DelayWorker,
+			Timeout: conf.TimeoutWorker,
+			Patient: rand.Int()%2 == 0}, taskListToWorkers, workersToItemList, workersStates[i], machines)
 	}
 
 	for i := 0; i < conf.Clients; i++ {
--- src/conf/config.go
+++ src/conf/config.go
@@ -5,9 +5,12 @@ import "time"
 const Verbose = true
 const DelayWorker = 100 * time.Millisecond
 const DelayClient = 5 * time.Second
+const DelayMachine = 100 * time.Millisecond
+const TimeoutWorker = 50 * time.Millisecond
 const TaskListSize = 10
 const ItemListSize = 10
-const Workers = 2
-const Clients = 1
+const Workers = 10
+const Clients = 3
+const Machines = 4
 const DelayCeoHi = 500 * time.Millisecond
 const DelayCeoLo = 100 * time.Millisecond
